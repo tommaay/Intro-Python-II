3e265af8e19f4bc78885816dce32c067584aba66
ToMMaaY <thuynh629@gmail.com>
2019-03-20 12:55:19 -0700

    created add_item method

--- src/item.py
+++ src/item.py
@@ -1,4 +1,14 @@
+from room import Room
+from player import Player
+
+
 class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def add_item(self, target):
+        if isinstance(target, Room) == True:
+            target['room_items'].append(self)
+        elif isinstance(target, Player) == True:
+            target['player_items'].append(self)
